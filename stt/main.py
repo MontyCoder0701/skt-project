c3b141a81c9ab971674c4a35c70a2c074556d85b
unknown <soojlee0106@naver.com>
2023-01-02 09:44:38 +0900

    Add basic STT code using naver clova

--- stt/main.py
+++ stt/main.py
@@ -1,3 +1,5 @@
+import requests
+import sys
 import librosa
 import speech_recognition as sr
 r = sr.Recognizer()
@@ -11,3 +13,21 @@ korean_audio = sr.AudioFile(
 with korean_audio as source:
     audio = r.record(source)
 r.recognize_google(audio_data=audio, language='ko-KR')
+
+
+client_id = "id 입력"
+client_secret = "비번 입력"
+lang = "Kor"  # 언어 코드 ( Kor, Jpn, Eng, Chn )
+url = "https://naveropenapi.apigw.ntruss.com/recog/v1/stt?lang=" + lang
+data = open('stt/sample.mp3', 'rb')
+headers = {
+    "X-NCP-APIGW-API-KEY-ID": client_id,
+    "X-NCP-APIGW-API-KEY": client_secret,
+    "Content-Type": "application/octet-stream"
+}
+response = requests.post(url,  data=data, headers=headers)
+rescode = response.status_code
+if (rescode == 200):
+    print(response.text)
+else:
+    print("Error : " + response.text)
