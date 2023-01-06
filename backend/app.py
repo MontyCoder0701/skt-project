54c6ab4ba17aa7328bbbea7b70385c514dd024f2
unknown <soojlee0106@naver.com>
2023-01-06 15:43:29 +0900

    Add Resnet backend code

--- backend/app.py
+++ backend/app.py
@@ -1,68 +1,85 @@
-from flask import Flask, request, jsonify, make_response
-from flask_restx import Api, Resource, fields
-import joblib
-import numpy as np
+from __future__ import division, print_function
+from keras.applications.resnet import ResNet50
+# coding=utf-8
 import sys
+import os
+import glob
+import re
+import numpy as np
+from keras.utils import load_img, img_to_array
 
-flask_app = Flask(__name__)
-app = Api(app=flask_app,
-          version="1.0",
-          title="Penguin identifier",
-          description="Predict the type of penguin")
+# Keras
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
 
-name_space = app.namespace('prediction', description='Prediction APIs')
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
 
-model = app.model('Prediction params',
-                  {'island': fields.Float(required=True,
-                                          description="island",
-                                          help="island cannot be blank"),
-                   'bill_length_mm': fields.Float(required=True,
-                                                  description="bill_length_mm",
-                                                  help="bill_length_mm cannot be blank"),
-                   'bill_depth_mm': fields.Float(required=True,
-                                                 description="bill_depth_mm",
-                                                 help="bill_depth_mm cannot be blank"),
-                   'flipper_length_mm': fields.Float(required=True,
-                                                     description="flipper_length_mm",
-                                                     help="flipper_length_mm cannot be blank"),
-                   'body_mass_g': fields.Float(required=True,
-                                               description="body_mass_g",
-                                               help="body_mass_g cannot be blank"),
-                   'sex': fields.Float(required=True,
-                                       description="sex",
-                                       help="sex cannot be blank")})
+# Define a flask app
+app = Flask(__name__)
 
-classifier = joblib.load('classifier.joblib')
+# Model saved with Keras model.save()
+MODEL_PATH = 'models/model_resnet.h5'
+
+# Load your trained model
+# model = load_model(MODEL_PATH)
+# model._make_predict_function()          # Necessary
+# print('Model loaded. Start serving...')
+
+# You can also use pretrained model from Keras
+# Check https://keras.io/applications/
+model = ResNet50(weights='imagenet')
+model.save('')
+print('Model loaded. Check http://127.0.0.1:5000/')
 
 
-@ name_space.route("/")
-class MainClass(Resource):
+def model_predict(img_path, model):
+    img = load_img(img_path, target_size=(224, 224))
 
-    def options(self):
-        response = make_response()
-        response.headers.add("Access-Control-Allow-Origin", "*")
-        response.headers.add('Access-Control-Allow-Headers', "*")
-        response.headers.add('Access-Control-Allow-Methods', "*")
-        return response
+    # Preprocessing the image
+    x = img_to_array(img)
+    # x = np.true_divide(x, 255)
+    x = np.expand_dims(x, axis=0)
 
-    @ app.expect(model)
-    def post(self):
-        try:
-            formData = request.json
-            data = [val for val in formData.values()]
-            prediction = classifier.predict(np.array(data).reshape(1, -1))
-            types = {0: "Adelie",
-                     1: "Chinstrap", 2: "Gentoo"}
-            response = jsonify({
-                "statusCode": 200,
-                "status": "Prediction made",
-                "result": "The type of penguin is: " + types[prediction[0]]
-            })
-            response.headers.add('Access-Control-Allow-Origin', '*')
-            return response
-        except Exception as error:
-            return jsonify({
-                "statusCode": 500,
-                "status": "Could not make prediction",
-                "error": str(error)
-            })
+    # Be careful how your trained model deals with the input
+    # otherwise, it won't make correct prediction!
+    x = preprocess_input(x, mode='caffe')
+
+    preds = model.predict(x)
+    return preds
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+
+        # Process your result for human
+        # pred_class = preds.argmax(axis=-1)            # Simple argmax
+        pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+        result = str(pred_class[0][0][1])               # Convert to string
+        return result
+    return None
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
